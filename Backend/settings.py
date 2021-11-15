d11280d19b804b84fda3fd9aebe346b430ac2117
c7w <admin@cc7w.cf>
2021-11-15 18:52:03 +0800

    update UI

--- Backend/settings.py
+++ Backend/settings.py
@@ -14,7 +14,7 @@ from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-WEB_URL = 'http://47.94.21.36:12000'
+WEB_URL = 'http://codeplay9.art:9000'
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
